38308b905518321a2ff2221d834cc13859789863
Lot Kyçyku <lotkyqyku@gmail.com>
2022-12-07 15:44:07 +0100

    Added change password feature on admin profile

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,7 @@ Route::get('/admin/dashboard',[AdminController::class,'AdminDashboard'])->name('
 Route::get('/admin/logout',[AdminController::class,'AdminDestroy'])->name('admin.logout');
 Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
 Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
+Route::get('/admin/change/password', [AdminController::class, 'AdminChangePassword'])->name('admin.change.password');
 
 });
 
