cd7accfff510c115de20206eaa25e9d7a0a1f7d1
Lot Kyçyku <lotkyqyku@gmail.com>
2022-12-07 10:52:08 +0100

    Admin login route and logout option

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,7 @@ require __DIR__.'/auth.php';
 // Admin Dashboard
 Route::middleware(['auth','role:admin'])->group(function(){
 Route::get('/admin/dashboard',[AdminController::class,'AdminDashboard'])->name('admin.dashboard');
+Route::get('/admin/logout',[AdminController::class,'AdminDestroy'])->name('admin.logout');
 
 });
 
@@ -43,3 +44,7 @@ Route::get('/admin/dashboard',[AdminController::class,'AdminDashboard'])->name('
 Route::middleware(['auth','role:vendor'])->group(function(){
 Route::get('/vendor/dashboard',[VendorController::class,'VendorDashboard'])->name('vendor.dashboard');
  });
+
+
+Route::get('/admin/login',[AdminController::class,'AdminLogin']);
+
